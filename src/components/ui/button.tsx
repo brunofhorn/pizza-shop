d9612afb669e584fe810ba56d6249590f36d9143
brunofhorn <b.fernandeshorn@gmail.com>
2025-06-02 00:46:38 -0300

    feat: add stale time to refetch

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -20,6 +20,8 @@ const buttonVariants = cva(
         ghost:
           'hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50',
         link: 'text-primary underline-offset-4 hover:underline',
+        success:
+          'bg-emerald-500 text-white shadow-xs hover:bg-emerald-500/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-emerald-600 dark:hover:bg-emerald-600/80',
       },
       size: {
         default: 'h-9 px-4 py-2 has-[>svg]:px-3',

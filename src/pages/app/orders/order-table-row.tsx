daf9fa3b058d7f9cf01430da24d32d3046692a9b
Bruno Fernandes Horn <b.fernandeshorn@gmail.com>
2025-06-06 00:32:02 -0300

    feat: change order table row

--- src/pages/app/orders/order-table-row.tsx
+++ src/pages/app/orders/order-table-row.tsx
@@ -1,16 +1,17 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query'
 import { formatDistanceToNow } from 'date-fns'
 import { ptBR } from 'date-fns/locale'
 import { ArrowRight, Search, X } from 'lucide-react'
+import { useState } from 'react'
 
+import { cancelOrder } from '@/api/cancel-order'
+import type { GetOrdersResponse } from '@/api/get-orders'
 import { OrderStatus } from '@/components/order-status'
 import { Button } from '@/components/ui/button'
 import { Dialog, DialogTrigger } from '@/components/ui/dialog'
 import { TableCell, TableRow } from '@/components/ui/table'
 
 import { OrderDetails } from './order-details'
-import { cancelOrder } from '@/api/cancel-order'
-import { queryClient } from '@/lib/react-query'
-import type { GetOrderDetailsResponse } from '@/api/get-order-details'
 export interface OrderTableRowProps {
   order: {
     orderId: string
@@ -25,78 +26,83 @@ export function OrderTableRow({ order }: OrderTableRowProps) {
   const [isDetailsOpen, setDetailsOpen] = useState(false)
   const queryClient = useQueryClient()
 
-  const { mutationAsync: cancelOrderFn } = useMutation({
+  const { mutateAsync: cancelOrderFn } = useMutation({
     mutationFn: cancelOrder,
-    async onSuccess(_, { orderId })
-      const ordersListCache = queryClient.getQueriesData<GetOrderResponse>({
-      queryKey: ['orders'],
-    })
+    async onSuccess(_, { orderId }) {
+      const ordersListCache = queryClient.getQueriesData<GetOrdersResponse>({
+        queryKey: ['orders'],
+      })
 
       ordersListCache.forEach(([cacheKey, cacheData]) => {
-      if (!cacheData) {
-        return
-      }
+        if (!cacheData) {
+          return
+        }
 
-      queryClient.setQueryData<GetOrderResponse>(cacheKey, {
-        ...cacheData,
-        orders: cacheData.orders.map(order => {
-          if (order.orderId === orderId) {
-            return {
-              ...order,
-              status: 'canceled',
+        queryClient.setQueryData<GetOrdersResponse>(cacheKey, {
+          ...cacheData,
+          orders: cacheData.orders.map((order) => {
+            if (order.orderId === orderId) {
+              return {
+                ...order,
+                status: 'canceled',
+              }
             }
-          }
-          return order
-        }),
+            return order
+          }),
+        })
       })
-    })
-  },
+    },
   })
 
-return (
-  <TableRow>
-    <TableCell>
-      <Dialog open={isDetailsOpen} onOpenChange={setDetailsOpen}>
-        <DialogTrigger asChild>
-          <Button variant="outline" size="xs">
-            <Search className="h-3 w-3" />
-            <span className="sr-only">Detalhes do pedido</span>
-          </Button>
-        </DialogTrigger>
-        <OrderDetails orderId={order.orderId} open={isDetailsOpen} />
-      </Dialog>
-    </TableCell>
-    <TableCell className="font-mono text-xs font-medium">
-      {order.orderId}
-    </TableCell>
-    <TableCell className="text-muted-foreground">
-      {formatDistanceToNow(order.createdAt, {
-        locale: ptBR,
-        addSuffix: true,
-      })}
-    </TableCell>
-    <TableCell>
-      <OrderStatus status={order.status} />
-    </TableCell>
-    <TableCell className="font-medium">{order.customerName}</TableCell>
-    <TableCell className="font-medium">
-      {(order.total / 100).toLocaleString('pt-BR', {
-        style: 'currency',
-        currency: 'BRL',
-      })}
-    </TableCell>
-    <TableCell>
-      <Button variant="outline" size="xs">
-        <ArrowRight className="mr-2 h-3 w-3" />
-        Aprovar
-      </Button>
-    </TableCell>
-    <TableCell>
-      <Button onClick={() => cancelOrderFn({ orderId: order.orderId })} variant="ghost" size="xs" disabled={['pending', 'processing'].includes(order.status)}>
-        <X className="mr-2 h-3 w-3" />
-        Cancelar
-      </Button>
-    </TableCell>
-  </TableRow>
-)
+  return (
+    <TableRow>
+      <TableCell>
+        <Dialog open={isDetailsOpen} onOpenChange={setDetailsOpen}>
+          <DialogTrigger asChild>
+            <Button variant="outline" size="xs">
+              <Search className="h-3 w-3" />
+              <span className="sr-only">Detalhes do pedido</span>
+            </Button>
+          </DialogTrigger>
+          <OrderDetails orderId={order.orderId} open={isDetailsOpen} />
+        </Dialog>
+      </TableCell>
+      <TableCell className="font-mono text-xs font-medium">
+        {order.orderId}
+      </TableCell>
+      <TableCell className="text-muted-foreground">
+        {formatDistanceToNow(order.createdAt, {
+          locale: ptBR,
+          addSuffix: true,
+        })}
+      </TableCell>
+      <TableCell>
+        <OrderStatus status={order.status} />
+      </TableCell>
+      <TableCell className="font-medium">{order.customerName}</TableCell>
+      <TableCell className="font-medium">
+        {(order.total / 100).toLocaleString('pt-BR', {
+          style: 'currency',
+          currency: 'BRL',
+        })}
+      </TableCell>
+      <TableCell>
+        <Button variant="outline" size="xs">
+          <ArrowRight className="mr-2 h-3 w-3" />
+          Aprovar
+        </Button>
+      </TableCell>
+      <TableCell>
+        <Button
+          onClick={() => cancelOrderFn({ orderId: order.orderId })}
+          variant="ghost"
+          size="xs"
+          disabled={['pending', 'processing'].includes(order.status)}
+        >
+          <X className="mr-2 h-3 w-3" />
+          Cancelar
+        </Button>
+      </TableCell>
+    </TableRow>
+  )
 }

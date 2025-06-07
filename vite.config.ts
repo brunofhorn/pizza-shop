2b14710229ad5e94d057e5c893419ad2bd1d9fed
Bruno Fernandes Horn <b.fernandeshorn@gmail.com>
2025-06-07 01:44:33 -0300

    feat: add tests suit

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,23 @@
 import path from 'node:path'
 
-import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
+import type { UserConfig } from 'vite'
 import { defineConfig } from 'vite'
+import type { InlineConfig } from 'vitest/node'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
+  test: {
+    globals: true,
+    setupFiles: ['./test/setup.ts'],
+    environment: 'happy-dom',
+  },
+} as UserConfig & {
+  test: InlineConfig
 })
